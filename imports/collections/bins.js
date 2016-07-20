6be732328980ab8e741a1fa42f5b1d5ab728de31
Keivan Shahida <keivanshahida@yahoo.com>
2016-07-20 11:42:30 -0400

    Sharing system added and fully functional

--- imports/collections/bins.js
+++ imports/collections/bins.js
@@ -15,7 +15,11 @@ Meteor.methods({
 	},
 
 	'bins.update': function(bin, content){
-		return Bins.update(bin._id, { $set: { content } });
+		return Bins.update(bin._id, {$set: {content}});
+	},
+
+	'bins.share': function(bin, email){
+		return Bins.update(bin._id, {$push: {sharedWith: email}});
 	}
 });
 
